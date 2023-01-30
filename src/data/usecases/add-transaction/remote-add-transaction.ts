55a2b79c9ff230ac6e0d51b7f59b76d16f21b663
João Vitor Lima <jvolima2004@gmail.com>
2023-01-30 11:17:26 -0300

    feat: ensure RemoteAddTransaction calls HttpPostClient with correct body

--- src/data/usecases/add-transaction/remote-add-transaction.ts
+++ src/data/usecases/add-transaction/remote-add-transaction.ts
@@ -1,12 +1,15 @@
+import { type AddTransactionParams } from '@/domain/usecases/add-transaction'
+
 export class RemoteAddTransaction {
   constructor (
     private readonly url: string,
     private readonly httpPostClient
   ) {}
 
-  async add (): Promise<void> {
+  async add (params: AddTransactionParams): Promise<void> {
     await this.httpPostClient.post({
-      url: this.url
+      url: this.url,
+      body: params
     })
   }
 }
