4539e2258099b86bdcb7f75bb241c578581a096e
João Vitor Lima <jvolima2004@gmail.com>
2023-01-30 11:38:27 -0300

    test: ensure RemoteAddTransaction throws UnexpectedError when HttpPostClient returns 400

--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -1,14 +1,21 @@
+import { type HttpResponse, type HttpPostClient, HttpStatusCode } from '../protocols/http'
+
 interface HttpPostParams {
   url: string
   body: any
 }
 
-export class HttpPostClientSpy {
+export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
   url?: string
-  body: any
+  body?: T
+  response: HttpResponse<R> = {
+    statusCode: HttpStatusCode.created
+  }
 
-  async post (params: HttpPostParams): Promise<void> {
+  async post (params: HttpPostParams): Promise<HttpResponse<R>> {
     this.url = params.url
     this.body = params.body
+
+    return this.response
   }
 }
--- src/data/usecases/add-transaction/remote-add-transaction.spec.ts
+++ src/data/usecases/add-transaction/remote-add-transaction.spec.ts
@@ -2,14 +2,17 @@ import { HttpPostClientSpy } from '@/data/test'
 import { RemoteAddTransaction } from './remote-add-transaction'
 import { faker } from '@faker-js/faker'
 import { mockAddTransactionParams } from '@/domain/test'
+import { type AddTransactionParams } from '@/domain/usecases/add-transaction'
+import { HttpStatusCode } from '@/data/protocols/http'
+import { UnexpectedError } from '@/domain/errors'
 
 type SutTypes = {
   sut: RemoteAddTransaction
-  httpPostClientSpy: HttpPostClientSpy
+  httpPostClientSpy: HttpPostClientSpy<AddTransactionParams, void>
 }
 
 const makeSut = (url: string = faker.internet.url()): SutTypes => {
-  const httpPostClientSpy = new HttpPostClientSpy()
+  const httpPostClientSpy = new HttpPostClientSpy<AddTransactionParams, void>()
   const sut = new RemoteAddTransaction(url, httpPostClientSpy)
 
   return {
@@ -32,5 +35,12 @@ describe('RemoteAddTransaction', () => {
     const { sut, httpPostClientSpy } = makeSut()
     await sut.add(transaction)
     expect(httpPostClientSpy.body).toEqual(transaction)
+  })
+
+  it('Should be able to throw UnexpectedError if HttpPostClient returns 400', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response.statusCode = HttpStatusCode.badRequest
+    const promise = sut.add(mockAddTransactionParams())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
