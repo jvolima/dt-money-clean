b23d31d2a27fffbb2a9d0a13a1ee0ff3a9a56022
João Vitor Lima <jvolima2004@gmail.com>
2023-02-01 10:07:29 -0300

    feat: newTransactionModal component

--- src/presentation/components/header/index.tsx
+++ src/presentation/components/header/index.tsx
@@ -5,6 +5,7 @@ import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 import logoImg from '../../assets/logo.svg'
 
 import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionModal } from '../newTransactionModal'
 
 export function Header (): JSX.Element {
   return (
@@ -16,6 +17,8 @@ export function Header (): JSX.Element {
           <Dialog.Trigger asChild>
             <NewTransactionButton>Nova transação</NewTransactionButton>
           </Dialog.Trigger>
+
+          <NewTransactionModal />
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
---
+++ src/presentation/components/newTransactionModal/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import * as Dialog from '@radix-ui/react-dialog'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles'
+
+export function NewTransactionModal (): JSX.Element {
+  return (
+    <Dialog.Portal>
+      <Overlay />
+
+      <Content>
+        <Dialog.Title>Nova transação</Dialog.Title>
+
+        <CloseButton>
+          <X size={24} />
+        </CloseButton>
+
+        <form>
+          <input
+            type="text"
+            placeholder="Descrição"
+            required
+          />
+          <input
+            type="number"
+            placeholder="Preço"
+            required
+          />
+          <input
+            type="text"
+            placeholder="Categoria"
+            required
+          />
+
+          <TransactionType>
+            <TransactionTypeButton variant="income" value="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+
+            <TransactionTypeButton variant="outcome" value="outcome">
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
+
+          <button type="submit">
+            Cadastrar
+          </button>
+        </form>
+      </Content>
+    </Dialog.Portal>
+  )
+}
