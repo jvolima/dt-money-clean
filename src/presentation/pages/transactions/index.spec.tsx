cee7be11c31020167176dc74560bf82a1c787029
João Vitor Lima <jvolima2004@gmail.com>
2023-02-01 19:24:30 -0300

    test: ensure Transactions load with correct initial state

--- src/presentation/pages/transactions/index.spec.tsx
+++ src/presentation/pages/transactions/index.spec.tsx
@@ -18,8 +18,31 @@ describe('Transactions component', () => {
       <Transactions />
     )
 
-    const category = sut.getByTestId('category')
+    const descriptionWrap = sut.getByTestId('description-wrap')
+    const description = sut.getByTestId('description')
+    const descriptionLabel = sut.getByTestId('description-label')
+    expect(descriptionWrap.getAttribute('data-status')).toBe('invalid')
+    expect(description.title).toBe('Campo obrigatório')
+    expect(descriptionLabel.title).toBe('Campo obrigatório')
 
-    expect(category)
+    const priceWrap = sut.getByTestId('price-wrap')
+    const price = sut.getByTestId('price')
+    const priceLabel = sut.getByTestId('price-label')
+    expect(priceWrap.getAttribute('data-status')).toBe('invalid')
+    expect(price.title).toBe('Campo obrigatório')
+    expect(priceLabel.title).toBe('Campo obrigatório')
+
+    const categoryWrap = sut.getByTestId('category-wrap')
+    const category = sut.getByTestId('category')
+    const categoryLabel = sut.getByTestId('category-label')
+    expect(categoryWrap.getAttribute('data-status')).toBe('invalid')
+    expect(category.title).toBe('Campo obrigatório')
+    expect(categoryLabel.title).toBe('Campo obrigatório')
+
+    const type = sut.getByTestId('type')
+    expect(type.title).toBe('Campo obrigatório')
+
+    const submitButton = sut.getByTestId('submit') as HTMLButtonElement
+    expect(submitButton.disabled).toBe(true)
   })
 })
