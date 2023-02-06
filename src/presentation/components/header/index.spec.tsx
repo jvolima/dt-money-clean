d39304cd057756b3f98d8ccbb3383401baeeeef3
João Vitor Lima <jvolima2004@gmail.com>
2023-02-05 22:47:18 -0300

    test: ensure Header open modal when click on new transaction button

---
+++ src/presentation/components/header/index.spec.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { fireEvent, render } from '@testing-library/react'
+import { Header } from '.'
+
+jest.mock('')
+
+describe('Header component', () => {
+  beforeAll(() => {
+    global.ResizeObserver = jest.fn().mockImplementation(() => ({
+      observe: jest.fn(),
+      unobserve: jest.fn(),
+      disconnect: jest.fn()
+    }))
+
+    HTMLDialogElement.prototype.show = jest.fn()
+    HTMLDialogElement.prototype.showModal = jest.fn()
+    HTMLDialogElement.prototype.close = jest.fn()
+  })
+
+  it('Should be able to open modal when click on new transaction button', () => {
+    const sut = render(
+      <Header />
+    )
+
+    const newTransactionButton = sut.getByTestId('new-transaction-button')
+    fireEvent.click(newTransactionButton)
+
+    expect(HTMLDialogElement.prototype.showModal).toHaveBeenCalled()
+  })
+})
