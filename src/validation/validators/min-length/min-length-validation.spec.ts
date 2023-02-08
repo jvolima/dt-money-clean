65588da5608fffa1a013fbc1ae177fca9c65d3f1
João Vitor Lima <jvolima2004@gmail.com>
2023-02-08 20:10:45 -0300

    test: ensure MinLengthValidation returns falsy if field does not exists in schema

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -21,4 +21,10 @@ describe('MinLengthValidation', () => {
     const error = sut.validate({ [field]: faker.random.alphaNumeric(5) })
     expect(error).toBeFalsy()
   })
+
+  it('Should be able to return falsy if field does not exists in schema', () => {
+    const sut = makeSut(faker.database.column())
+    const error = sut.validate({ [faker.database.column()]: faker.random.alphaNumeric(5) })
+    expect(error).toBeFalsy()
+  })
 })
