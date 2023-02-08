d1d2175ca8f42e6f209c811d0c189a14c03b93b7
João Vitor Lima <jvolima2004@gmail.com>
2023-02-08 20:09:55 -0300

    feat: ensure MinLengthValidation returns falsy if field does not exists in schema

--- src/validation/validators/min-length/min-length-validation.ts
+++ src/validation/validators/min-length/min-length-validation.ts
@@ -2,9 +2,9 @@ import { InvalidFieldError } from '@/validation/errors'
 import { type FieldValidation } from '@/validation/protocols/field-validation'
 
 export class MinLengthValidation implements FieldValidation {
-  constructor (readonly field: string, readonly count: number) {}
+  constructor (readonly field: string, readonly minLength: number) {}
 
   validate (input: object): Error {
-    return input[this.field]?.length >= this.count ? null : new InvalidFieldError()
+    return input[this.field]?.length < this.minLength ? new InvalidFieldError() : null
   }
 }
