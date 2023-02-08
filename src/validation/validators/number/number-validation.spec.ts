b81752dce7364131d2e9749fd5fa324d0f002b10
João Vitor Lima <jvolima2004@gmail.com>
2023-02-08 20:17:29 -0300

    test: ensure NumberValidation returns error if value is not a number

---
+++ src/validation/validators/number/number-validation.spec.ts
@@ -0,0 +1,12 @@
+import { InvalidFieldError } from '@/validation/errors'
+import { faker } from '@faker-js/faker'
+import { NumberValidation } from './number-validation'
+
+describe('NumberValidation', () => {
+  it('Should be able to return error if value is not a number', () => {
+    const field = faker.database.column()
+    const sut = new NumberValidation(field)
+    const error = sut.validate({ [field]: faker.random.word() })
+    expect(error).toEqual(new InvalidFieldError())
+  })
+})
