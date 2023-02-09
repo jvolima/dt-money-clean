cb1e90f1e10bb5a812f4224ca56369d48677d812
João Vitor Lima <jvolima2004@gmail.com>
2023-02-08 21:31:11 -0300

    test: ensure ValidationBuilder returns MinLengthValidation

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,4 +1,5 @@
 import { faker } from '@faker-js/faker'
+import { MinLengthValidation } from '../min-length/min-length-validation'
 import { RequiredFieldValidation } from '../required-field/required-field-validation'
 import { ValidationBuilder as sut } from './validation-builder'
 
@@ -7,5 +8,11 @@ describe('ValidationBuilder', () => {
     const field = faker.database.column()
     const validations = sut.field(field).required().build()
     expect(validations).toEqual([new RequiredFieldValidation(field)])
+  })
+
+  it('Should be able to return MinLengthValidation', () => {
+    const field = faker.database.column()
+    const validations = sut.field(field).min(5).build()
+    expect(validations).toEqual([new MinLengthValidation(field, 5)])
   })
 })
