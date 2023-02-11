e8687fa05d89f61eec2913c76c1fd63f72de6484
João Vitor Lima <jvolima2004@gmail.com>
2023-02-11 19:50:23 -0300

    test: ensure RemoteLoadTransactions returns a list of TransactionModel if HttpGetClient returns 200

--- src/data/usecases/load-transactions/remote-load-transactions.ts
+++ src/data/usecases/load-transactions/remote-load-transactions.ts
@@ -1,17 +1,18 @@
 import { HttpStatusCode, type HttpGetClient } from '@/data/protocols/http'
 import { UnexpectedError } from '@/domain/errors'
 import { type TransactionModel } from '@/domain/models'
+import { type LoadTransactions } from '@/domain/usecases'
 
-export class RemoteLoadTransactions {
+export class RemoteLoadTransactions implements LoadTransactions {
   constructor (
     private readonly url: string,
     private readonly httpGetClient: HttpGetClient<TransactionModel[]>
   ) {}
 
-  async loadAll (): Promise<void> {
+  async loadAll (): Promise<TransactionModel[]> {
     const httpResponse = await this.httpGetClient.get({ url: this.url })
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: break
+      case HttpStatusCode.ok: return httpResponse.body
       default: throw new UnexpectedError()
     }
   }
