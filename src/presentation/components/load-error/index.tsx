3af894e1eefd85f8be8fa2beff36aa71e9a3611a
João Vitor Lima <jvolima2004@gmail.com>
2023-02-14 09:40:55 -0300

    feat: ensure Transactions calls LoadTransactions on reload

--- src/presentation/components/load-error/index.tsx
+++ src/presentation/components/load-error/index.tsx
@@ -3,11 +3,19 @@ import React, { useContext } from 'react'
 import { LoadErrorContainer } from './styles'
 
 export function LoadError (): JSX.Element {
-  const { state } = useContext(TransactionsContext)
+  const { state, setState } = useContext(TransactionsContext)
+
+  function reload (): void {
+    setState({
+      ...state,
+      reload: !state.reload
+    })
+  }
 
   return (
     <LoadErrorContainer data-testid="error">
       <span>{state.error}</span>
+      <button onClick={reload} data-testid="reload">Tentar novamente</button>
     </LoadErrorContainer>
   )
 }
